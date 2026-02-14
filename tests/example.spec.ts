88392da934f9a1c6fbd2f655caa1f3bc16b3487d
DmitriiG8421 <golikov.dima15@gmail.com>
2026-02-14 12:12:34 +1100

    Deleted any unecessary tests

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,35 +16,6 @@ test('has title1', async ({ page }) => {
 });
 
 
-test('has title2', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("");
-});
-
-test('has title3', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("");
-});
-
-test('has title4', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("");
-});
-
-test('has title5', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle("");
-});
-
-
 
 
 

7fc6d1a954619a7fe9b085370e3efd1f6a3fba29
DmitriiG8421 <golikov.dima15@gmail.com>
2026-02-28 14:01:26 +1100

    Ran the code.

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,19 +3,39 @@ import { test, expect } from '@playwright/test';
 
 
 test('has title', async ({ page }) => {
-
   await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle(/Playwright/);
+});
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Fast.*reliable.*modern.*Playwright$/);
-    await expect(page).toHaveTitle(/testing/);
-      await expect(page).toHaveTitle(/testing|modern/);
+test('has title1', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+});
 
+test('has title2', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
   await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
 });
 
+test('has title3', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+});
 
+test('has title4', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+});
 
+test('has title5', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+});
+
+test('has title6', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await expect(page).toHaveTitle("Fast and reliable end-to-end testing for modern web apps | Playwright");
+});
 
 
 

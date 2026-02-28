2cda0c2f983f3ddf11c6bd9298c631ac84419967
DmitriiG8421 <golikov.dima15@gmail.com>
2026-02-28 12:41:49 +1100

    Added runTime field to summary object

--- summary-reporter.js
+++ summary-reporter.js
@@ -1,21 +1,32 @@
 const fs = require('fs');
+
 class SummaryReporter {
-  onBegin(config, suite) { this.rootSuite = suite; }
-  onEnd(result) {
-    const summary = {
-      schemaVersion: 1, passed: 0, failed: 0, flaky: 0, total: 0,
-      startTime: new Date().toISOString(), isSummary: true
-    };
-    if (this.rootSuite) {
-      for (const test of this.rootSuite.allTests()) {
-        const out = test.outcome();
-        if (out === 'expected') summary.passed++;
-        if (out === 'unexpected') summary.failed++;
-        if (out === 'flaky') summary.flaky++;
-      }
+
+    onBegin(config, suite) {
+        this.rootSuite = suite;
     }
-    summary.total = summary.passed + summary.failed + summary.flaky;
-    fs.writeFileSync('test-summary.json', JSON.stringify(summary, null, 2));
-  }
+
+
+    onEnd(result) {
+        const summary = {
+            schemaVersion: 1, passed: 0, failed: 0, flaky: 0, total: 0,
+            startTime: new Date().toISOString(), isSummary: true, runTime: 0
+        };
+
+        if (this.rootSuite) {
+
+            for (const test of this.rootSuite.allTests()) {
+                const out = test.outcome();
+                if (out === 'expected') summary.passed++;
+                if (out === 'unexpected') summary.failed++;
+                if (out === 'flaky') summary.flaky++;
+            }
+            summary.runTime = result.duration;
+        }
+        summary.total = summary.passed + summary.failed + summary.flaky;
+        fs.writeFileSync('test-summary.json', JSON.stringify(summary, null, 2));
+        console.log(summary);
+    }
 }
+
 module.exports = SummaryReporter;
